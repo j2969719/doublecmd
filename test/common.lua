a7d5e342658aac0a8f5a1cde96e84cc9cdfa5af9
j2969719 <j172933@mvrht.net>
2024-01-31 10:12:41 +0300

    ...

--- test/common.lua
+++ test/common.lua
@@ -89,6 +89,83 @@ local define =
   ID_CLOSE = 8,
   ID_HELP = 9,
 
+  -- LogWrite
+  lmsgInfo = 0,
+  lmsgSuccess = 1,
+  lmsgError = 2,
+  -- Active panel
+  apLeft = 0,
+  apRight = 1,
+
+  -- Attr
+  faReadOnly = 0x00000001, -- The file is read-only.
+  faHidden = 0x00000002, -- The file is hidden. In Unix/Linux, this means that the filename starts with a dot.
+  faSysFile = 0x00000004, -- The file is a system file. In Unix/Linux, this means that the file is a character, block or FIFO file.
+  faVolumeId = 0x00000008, -- Volume Label. Only for DOS/Windows on a plain FAT (not VFAT or FAT32) filesystem.
+  faDirectory = 0x00000010, -- File is a directory.
+  faArchive = 0x00000020, -- File is archived. Not possible in Unix/Linux.
+  faSymLink = 0x00000400, -- File is a symbolic link.
+
+  -- WDX
+
+  ft_nomorefields = 0,
+  ft_numeric_32, 1,
+  ft_numeric_64, 2,
+  ft_numeric_floating = 3,
+  ft_date = 4,
+  ft_time = 5,
+  ft_boolean = 6,
+  ft_multiplechoice = 7,
+  ft_string = 8,
+  ft_fulltext = 9,
+  ft_datetime = 10,
+  ft_stringw = 11,
+  ft_fulltextw = 12,
+
+  ft_comparecontent = 100,
+
+  -- for ContentGetValue
+  ft_nosuchfield = -1, -- error, invalid field number given
+  ft_fileerror = -2, -- file i/o error
+  ft_fieldempty = -3, -- field valid, but empty
+  ft_ondemand = -4, -- field will be retrieved only when user presses <SPACEBAR>
+  ft_notsupported = -5, -- function not supported
+  ft_setcancel = -6, -- user clicked cancel in field editor
+  ft_delayed = 0, -- field takes a long time to extract = -> try again in background
+  -- for ContentFindValue:
+  ft_found = 1, -- Value was found
+  ft_notfound = -8, -- Value was NOT found
+
+  -- for ContentSetValue
+  ft_setsuccess = 0, -- setting of the attribute succeeded
+
+  -- for ContentGetSupportedFieldFlags
+  contflags_edit = 1,
+  contflags_substsize = 2,
+  contflags_substdatetime = 4,
+  contflags_substdate = 6,
+  contflags_substtime = 8,
+  contflags_substattributes = 10,
+  contflags_substattributestr = 12,
+  contflags_passthrough_size_float = 14,
+  contflags_substmask = 14,
+  contflags_fieldedit = 16,
+  contflags_fieldsearch = 32,
+  contflags_searchpageonly = 64,
+
+  contst_readnewdir = 1,
+  contst_refreshpressed = 2,
+  contst_showhint = 4,
+
+  setflags_first_attribute = 1, -- First attribute of this file
+  setflags_last_attribute = 2, -- Last attribute of this file
+  setflags_only_date = 4, -- Only set the date of the datetime value!
+
+  editflags_initialize = 1, -- The data passed to the plugin may be used to initialize the edit dialog
+
+  CONTENT_DELAYIFSLOW = 1, -- ContentGetValue called in foreground
+  CONTENT_PASSTHROUGH = 2, -- If requested via contflags_passthrough_size_float: The size is passed in as floating value, TC expects correct value m from the given units value, and optionally a text string
+
 }
 
 return define
